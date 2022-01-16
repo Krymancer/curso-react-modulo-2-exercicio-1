e51983990d07392fb23c9d8bf10cb80ad8114142
Júnior Nascimento <junior.nascm@gmail.com>
2022-01-16 10:02:26 -0300

    init

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,75 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./index.scss";
 
 type Props = {};
 
-type State = {
-  title: string;
-  n1: number;
-  n2: number;
-  result: number;
-};
+const Main: React.FC<Props> = () => {
+  const title = process.env.REACT_APP_TITLE || "Title not found in env file";
+  const [n1, sn1] = useState<number>(0);
+  const [n2, sn2] = useState<number>(0);
+  const [r, sr] = useState<number>(0);
 
-class Main extends React.Component<Props, State> {
-  state: State = {
-    title: process.env.REACT_APP_TITLE || "Title not found in env file",
-    n1: 0,
-    n2: 0,
-    result: 0,
-  };
-
-  sn1(number: number) {
-    this.setState((state) => ({
-      n1: number,
-    }));
-  }
-
-  sn2(number: number) {
-    this.setState((state) => ({
-      n2: number,
-    }));
-  }
-
-  sr(number: number) {
-    this.setState((state) => ({
-      result: number,
-    }));
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <h1>{this.state.title}</h1>
-        <div className="content">
-          <div>
-            <input
-              type="number"
-              onChange={(e) => {
-                this.sn1(parseInt(e.target.value));
-              }}
-              value={this.state.n1}
-            />
-            <input
-              type="number"
-              onChange={(e) => {
-                this.sn2(parseInt(e.target.value));
-              }}
-              value={this.state.n2}
-            />
-          </div>
-          <button
-            onClick={() => {
-              this.sr(this.state.n1 + this.state.n2);
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <div className="content">
+        <div>
+          <input
+            type="number"
+            onChange={(e) => {
+              sn1(parseInt(e.target.value));
             }}
-          >
-            +
-          </button>
-          <span>{this.state.result}</span>
+            value={n1}
+          />
+          <input
+            type="number"
+            onChange={(e) => {
+              sn2(parseInt(e.target.value));
+            }}
+            value={n2}
+          />
         </div>
+        <button
+          onClick={() => {
+            sr(n1 + n2);
+          }}
+        >
+          +
+        </button>
+        <span>{r}</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Main;
